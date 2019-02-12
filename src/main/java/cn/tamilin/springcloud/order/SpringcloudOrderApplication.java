e80908847ce09cb6bc5150e6351984c10c42e982
924494975@qq.com <924494975@qq.com>
2019-02-12 17:14:06 +0800

    feign-hystrix 和 hystrixDashboard

--- src/main/java/cn/tamilin/springcloud/order/SpringcloudOrderApplication.java
+++ src/main/java/cn/tamilin/springcloud/order/SpringcloudOrderApplication.java
@@ -6,12 +6,16 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication
 //@EnableDiscoveryClient
 @EnableFeignClients
 //@EnableCircuitBreaker
 @SpringCloudApplication
+@ComponentScan(basePackages = "cn.tamilin") //配置包扫描
+//@EnableHystrixDashboard // hystrixdashboard 配置好依赖后要加注解
 public class SpringcloudOrderApplication {
 
 	public static void main(String[] args) {
--- src/main/java/cn/tamilin/springcloud/order/client/ProductClient.java
+++ src/main/java/cn/tamilin/springcloud/order/client/ProductClient.java
@@ -3,6 +3,7 @@ package cn.tamilin.springcloud.order.client;
 import cn.tamilin.springcloud.order.dataobject.ProductInfo;
 import cn.tamilin.springcloud.order.dto.CartDTO;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +12,11 @@ import java.util.List;
 
 /**
  * @ClassName ProductClient
- * @Desciption TODO
+ * @Desciption 关于product的应该写在product项目里面 分模块去写 然后jar包引入到order项目里面
  * @Author summer
  * @Date 2019/2/4 12:08
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product" ,fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
 	@GetMapping("/msg")
@@ -26,4 +27,26 @@ public interface ProductClient {
 
 	@PostMapping("/decreaseStock")
 	void decreaseStock(@RequestBody List<CartDTO> cartDTOList);
+
+
+	/**
+	 * 在order服务中调用product服务的时候 如果product服务没有启动的话 调用url会用到下面的这个方法 返回null
+	 */
+	@Component
+	static class ProductClientFallback  implements ProductClient {
+		@Override
+		public String msg() {
+			return null;
+		}
+
+		@Override
+		public List<ProductInfo> listForOrder(List<String> productList) {
+			return null;
+		}
+
+		@Override
+		public void decreaseStock(List<CartDTO> cartDTOList) {
+
+		}
+	}
 }
